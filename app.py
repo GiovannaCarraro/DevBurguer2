4ee585fc80b9d568d321a3fbaceea3849f4a82de
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-04-09 16:29:00 -0300

    fazendo

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+from model.produto import recuperar
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return render_template("index.html")
+    produtos = recuperar()
+    return render_template("index.html", produtos = produtos)
 
 @app.route("/produto")
 def pagina_produto():
+    
     return render_template("produto.html")
 
 app.run(debug=True)
---
+++ model/produto.py
@@ -0,0 +1,17 @@
+from  database.conexao import conectar
+
+def recuperar():
+    # passo 1 e 2 ja feito
+        conexao, cursor = conectar()
+
+        cursor.execute("SELECT codigo, produto, descricao, destaque, preco, foto, disponibilidade FROM Produtos;")
+  
+        #    cursor.execute("SELECT codigo, produto, descricao, preco, foto FROM Produtos")
+
+        #rec os dados
+        produtos =  cursor.fetchall()
+
+        # fechar a conexão
+        conexao.close()
+
+        return produtos
