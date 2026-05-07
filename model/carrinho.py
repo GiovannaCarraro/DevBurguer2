1a05c8fe26b8f12ac1a47ce5411fc69e74a27787
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-05-07 15:32:15 -0300

    insert

--- model/carrinho.py
+++ model/carrinho.py
@@ -17,3 +17,31 @@ def recuperar_carrinho(usuario:str) ->list:
     resultado = cursor.fetchall()
     conexao.close()
     return resultado
+
+def inserir_item(usuario, cod_produto, disponibilidade):
+    conexao, cursor = conectar()
+    cursor.execute("""
+               SELECT COD_ITENS_CARRINHOS
+                FROM Itens_Carrinhos, Carrinhos
+                WHERE Itens_Carrinhos.COD_CARRINHO = Carrinhos.COD_CARRINHO
+                AND Carrinhos.USUARIO = %s
+                AND Carrinhos.FINALIZADO = 0
+                LIMIT 1;""", [usuario])
+    
+    resultado_carrinho = cursor.fetchone()
+
+    if resultado_carrinho:
+        codigo_carrinho = resultado_carrinho["COD_CARRINHO"]
+    else:
+        cursor.execute("""INSERT INTO Carrinhos (usuario)
+                       VALUES (%s);""", [usuario])
+        
+        codigo_carrinho = cursor.lastrowid()
+
+        cursor.execute("""
+                    INSERT INTO Itens_Carrinhos 
+                    (COD_CARRINHO, COD_PRODUTO, QUANTIDADE)
+                    VALUES (%s, %s, %s);""", [codigo_carrinho, cod_produto, disponibilidade])
+        
+        conexao.commit()
+    conexao.close()
---
+++ teste.py
@@ -0,0 +1,3 @@
+from model.carrinho import inserir_item
+
+inserir_item("godothalya", 2, 4)
